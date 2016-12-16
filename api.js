8311e5886bd47c85b50eab282f70216a95185d6c
Azim Uddin <Azim@guest-2-00664.dhcp.unc.edu>
2016-12-15 21:16:51 -0500

    Basic js file

---
+++ api.js
@@ -0,0 +1,17 @@
+
+var searchTerm = $("#box1").val();
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=a1adcf23b0b64f0a9c094bbb2617804e&?q=" +
+        searchTerm ;
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+
+      .done(function(response) {
+      	console.log(response);
+      }
+
+
+
+      // https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=a1adcf23b0b64f0a9c094bbb2617804e&?q=apple
